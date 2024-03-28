99fc6edfd54ecdaae7e5724d0a72cb506ba0bd52
ayuhzkishan <ayush.kishan29@gmail.com>
2024-03-28 09:42:12 +0530

    question 1 corrected

--- LOC G5 week 1 assignment/Q1.cpp
+++ LOC G5 week 1 assignment/Q1.cpp
@@ -3,7 +3,10 @@ using namespace std;
 int main(){
     int m=1;
     while (m<=100){
-        if (m%3==0)
+        if (m%3==0 && m%5==0){
+          cout << "fizzbuzz" <<endl;
+        }
+        else if (m%3==0)
           { cout<< "fizz"<<endl;}
         else if (m%5==0){
             cout <<"buzz"<< endl;
