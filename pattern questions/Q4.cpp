163aa91403b21c3aa9ba3c0c44dd42a15cbe16b5
ayuhzkishan <ayush.kishan29@gmail.com>
2024-03-30 11:23:40 +0530

    pattern questions completed

--- pattern questions/Q4.cpp
+++ pattern questions/Q4.cpp
@@ -7,14 +7,18 @@
 using namespace std;
 
 int main(){
-    int n=3;
-    //cout<<"Enter the number of rows in pattern"<< endl;
-    //cin >> n;
+    int n;
+    cout<<"Enter the number of rows in pattern"<< endl;
+    cin >> n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=i;j++){
-           for(int m=1;m<=i;m++){
-                 cout<<j;
-           }
-        }cout<<endl;
+           cout << 1;
+        }
+        for(int k=1;k<=i;k++){
+           cout << 2;
+        }
+        for(int l=1;l<=i;l++){
+           cout << 3;
+        }cout << endl;    
     }
 }
